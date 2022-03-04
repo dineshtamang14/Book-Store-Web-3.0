9a2336e49d535ab8065c7d79c3ec283fdd72b023
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-03-04 17:43:30 +0530

    update api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,76 +1,103 @@
-import { PageHeader, Button, Input, Space, Badge } from 'antd';
+import { PageHeader, Button, Input, Space, Badge } from "antd";
 import { useMoralis } from "react-moralis";
-import { Link } from 'react-router-dom';
-import './Header.css'
+import { Link } from "react-router-dom";
+import "./Header.css";
 // import Amazon from "../images/logo.png";
 // import USA from "../images/usa.png";
 import BookStore from "../images/bookstore.png";
-import {ShoppingCartOutlined, MenuOutlined} from "@ant-design/icons";
+import { ShoppingCartOutlined, MenuOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
-const {Search } = Input;
-const categories = ["Comics", "Dictionaries", "Novels", "Fantasy", "Horror", "Adventure"];
+const { Search } = Input;
+const categories = [
+  "Python",
+  "Mongodb",
+  "PHP",
+  "Engineering",
+  "MySQL",
+  "Linux",
+  "DSA",
+];
 
 const Header = () => {
   const { authenticate, account } = useMoralis();
-  return(
+  const [search, setSearch] = useState("");
+  const [next, setNext] = useState(false);
+  console.log(search);
+
+  const searchBook = (e) => {
+    e.preventDefault();
+    setNext(true);
+  };
+
+  return (
     <div className="site-page-header-ghost-wrapper">
       <div className="header">
-      <PageHeader
-        ghost={false}
-        extra={[
-          <>
-          <Link to="/"><img src={BookStore} className="logo" alt="logo"></img></Link>
-          {/* <img src={BookStore} className="logo"></img> */}
-          <Search
-              placeholder="Find A Product"
-              enterButton
-              className = "searchBar"
-            />
-         <Button 
-         className="login header-btn"
-         key="1" 
-         type="primary" 
-         onClick={() => authenticate()}>
-          {account ? <span>{account.slice(0,5)}...</span> : <span>login</span>}
-          </Button>
-          <div className="header-cart">
-          <Space size={"large"}>
-              
-              <Badge count={0} showZero>
-                <span className="header-buttons">
-                  <ShoppingCartOutlined className="header-icon" />
-                  Cart
-                </span>
-              </Badge>
-              {/* <Space className="header-buttons" size={"small"}>
+        <PageHeader
+          ghost={false}
+          extra={[
+            <>
+              <Link to="/">
+                <img src={BookStore} className="logo" alt="logo"></img>
+              </Link>
+              {/* <img src={BookStore} className="logo"></img> */}
+              <form className="searchBar">
+                <Search
+                  placeholder="Find A Product"
+                  enterButton
+                  className="searchBar"
+                  onChange={(e) => setSearch(e.target.value)}
+                  value={search}
+                />
+                <button type="button" onSubmit={searchBook} hidden />
+              </form>
+              <Button
+                className="login header-btn"
+                key="1"
+                type="primary"
+                onClick={() => authenticate()}
+              >
+                {account ? (
+                  <span>{account.slice(0, 5)}...</span>
+                ) : (
+                  <span>login</span>
+                )}
+              </Button>
+              <div className="header-cart">
+                <Space size={"large"}>
+                  <Badge count={0} showZero>
+                    <span className="header-buttons">
+                      <ShoppingCartOutlined className="header-icon" />
+                      Cart
+                    </span>
+                  </Badge>
+                  {/* <Space className="header-buttons" size={"small"}>
                 <img src={USA} alt="region" className="flag"></img>▾
               </Space> */}
-              
-            </Space>
-          </div>
-          </>
-        ]}>
-      </PageHeader>
+                </Space>
+              </div>
+            </>,
+          ]}
+        ></PageHeader>
       </div>
-      
-      <div className="site-page-subheader-ghost-wrapper">
-      <Space size={"middle"}>
-        <Space size={"small"} style={{fontWeight:"bold"}}>
-          <MenuOutlined />
-          Categories
-        </Space>
-        {categories.map((e) =>{
-          return(
-            <Link to="/categories" state={e} className="categories">
-              {e}
-            </Link>
-          )
 
-        })}
-      </Space>
+      <div className="site-page-subheader-ghost-wrapper">
+        <Space size={"middle"}>
+          <Space size={"small"} style={{ fontWeight: "bold" }}>
+            <MenuOutlined />
+            Categories
+          </Space>
+          {categories.map((e) => {
+            return (
+              <Link to="/categories" state={e} className="categories">
+                {e}
+              </Link>
+            );
+          })}
+        </Space>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Header;
--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Button, Layout } from "antd";
 import { useLocation } from "react-router";
 import Header from "../components/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Rating from "../components/Rating";
 import PriceRanges from "../components/PriceRanges";
 import Results from "../components/Results";
+import axios from "../axios";
 
 const { Sider, Content } = Layout;
 
@@ -14,6 +15,18 @@ const Categories = () => {
   const [rating, setRating] = useState(1);
   const [priceMin, setPriceMin] = useState(0);
   const [priceMax, setPriceMax] = useState(100);
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(`/products?search=${category}`);
+      setBooks(request.data.Books);
+    }
+    fetchData();
+  }, [category]);
+
+  console.log(category);
 
   return (
     <>
@@ -27,9 +40,14 @@ const Categories = () => {
         <Layout>
           <Sider width="340px" theme="light" style={{ padding: "25px" }}>
             <div style={{ display: "flex", flexDirection: "column" }}>
-            <Rating rating={rating} setRating={setRating} />
-            <PriceRanges priceMin={priceMin} setPriceMin={setPriceMin} priceMax={priceMax} setPriceMax={setPriceMax}/>
-            <Button className="login">Apply Filters</Button>
+              <Rating rating={rating} setRating={setRating} />
+              <PriceRanges
+                priceMin={priceMin}
+                setPriceMin={setPriceMin}
+                priceMax={priceMax}
+                setPriceMax={setPriceMax}
+              />
+              <Button className="login">Apply Filters</Button>
             </div>
           </Sider>
 
@@ -37,12 +55,16 @@ const Categories = () => {
             theme="light"
             style={{ padding: "35px", backgroundColor: "white" }}
           >
-            <h1 style={{fontSize:"30px"}}>RESULTS</h1>
-            <Results category={category} rating={rating} priceMin={priceMin} priceMax={priceMax}/>
+            <h1 style={{ fontSize: "30px" }}>RESULTS</h1>
+            <Results
+              // category={category}
+              // rating={rating}
+              // priceMin={priceMin}
+              // priceMax={priceMax}
+              books={books}
+            />
           </Content>
         </Layout>
-
-        
       </div>
     </>
   );
